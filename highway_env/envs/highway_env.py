cd51342c55d995501f69fbfb463925acd9be91f7
ouaburst <45198357+ouaburst@users.noreply.github.com>
2021-05-17 21:00:25 +0200

    Update highway_env.py
    Change code for assignment 3.

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -81,6 +81,8 @@ class HighwayEnv(AbstractEnv):
         :param action: the last action performed
         :return: the corresponding reward
         """
+        lane_change = action == 0 or action == 2
+
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
@@ -88,9 +90,11 @@ class HighwayEnv(AbstractEnv):
         reward = \
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
-            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
+            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1) \
+            + self.config["lane_change_reward"] * lane_change
+
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"],
+                          [self.config["collision_reward"] + self.config["lane_change_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
